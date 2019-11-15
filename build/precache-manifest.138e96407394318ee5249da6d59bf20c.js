a0c9e9fb2c8e34cdeb05b64063eabd7cdaf8dc6e
Artur <arturl@techfinancials.com>
2019-11-15 21:33:43 +0200

    build version push

---
+++ build/precache-manifest.138e96407394318ee5249da6d59bf20c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbd247fa25317e8984bd6c9d2491a92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d992b1fdba6b1f228683",
+    "url": "/static/css/main.ede6f10f.chunk.css"
+  },
+  {
+    "revision": "229a33be9fa7fb33c023",
+    "url": "/static/js/2.45b6c09d.chunk.js"
+  },
+  {
+    "revision": "d992b1fdba6b1f228683",
+    "url": "/static/js/main.bcd01113.chunk.js"
+  },
+  {
+    "revision": "195123c7f36e6a5b6bf3",
+    "url": "/static/js/runtime-main.a74b1b2e.js"
+  }
+]);
